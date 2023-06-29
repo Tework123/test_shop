8fc949be63a7426902660cb7deba4a9c2f54509b
Tework123 <daniliesaec@gmail.com>
2023-06-29 17:21:23 +0300

    add get

--- backend/main.py
+++ backend/main.py
@@ -1,15 +1,18 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route('/', methods=['GET'])
 def index():
     a = []
     for i in range(100):
         a.append(str(i))
-    return render_template('index.html', a=''.join(a))
+
+    response = jsonify({'data': a})
+    response.status_code = 200
+    return response
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
