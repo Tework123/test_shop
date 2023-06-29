97066b36522027dc9f0efd447d05b58352629772
FalconBone <happycatvasiliy9786@gmail.com>
2023-06-29 17:20:45 +0300

    get post front

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
+import axios from 'axios'
 import './App.css';
 
 function App() {
+
+  const [message, setMessage] = useState(null);
+
+
+  useEffect(() => {
+    axios.get(``).then((response) => {
+      setMessage(response.data);
+    });
+  }, []);
+
+  function createPost() {
+    axios
+      .post('/', {
+        message: 'suda naher'
+      })
+      .then((response) => {
+        setMessage(response.data);
+      });
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {message}
+      <button onClick={createPost}>get message</button>
     </div>
   );
 }
